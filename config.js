5c740aaec60ed58f6a3c73192b99dfb267b89f6e
jywan <jywan@ctrip.com>
2018-11-28 19:44:18 +0800

    init database

---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+
+    port: 3001,
+
+    database: {
+        DATABASE: 'test',
+        USERNAME: 'root',
+        PASSWORD: '87713389',
+        PORT: '3306',
+        HOST: 'localhost'
+    }
+}
+
+module.exports = config
